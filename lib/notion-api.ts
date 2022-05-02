2a1de714304adc8e340b65d993b6d93f306d18f5
byung ju choi <bjc30849@gmail.com>
2022-05-02 21:16:24 +0900

    feat: tailwind/css 도입

--- lib/notion-api.ts
+++
@@ -1,8 +0,0 @@
-import { NotionAPI } from 'notion-client'
-import { Client } from '@notionhq/client'
-
-export const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
-})
-
-export const notionAPI = new NotionAPI()
