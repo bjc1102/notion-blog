d76405d3f826767833586d7dee0667609f1e00af
byung ju choi <bjc30849@gmail.com>
2022-07-07 21:34:07 +0900

    chore: 컬러 수정:

--- pages/search.tsx
+++ pages/search.tsx
@@ -44,7 +44,7 @@ const Search: NextPage<Props> = ({
     <div className="max-w-3xl mx-auto mt-24 py-8 px-12 box-border">
       <div className="flex flex-col overflow-hidden">
         <input
-          className="border-2 text-sm rounded-lg block w-full p-2.5 pl-12 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none"
+          className="border-2 text-sm rounded-lg block w-full p-2.5 pl-12 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-accent focus:border-accent outline-none"
           placeholder="검색어를 입력하세요"
           onChange={handleChange}
           value={search}
