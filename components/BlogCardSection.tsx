85c4632da2a6a90e135e098f9c9cc35ce51e34bd
byung ju choi <bjc30849@gmail.com>
2022-07-19 19:37:24 +0900

    feat: 포스트가 없을 때 처리

--- components/BlogCardSection.tsx
+++ components/BlogCardSection.tsx
@@ -10,14 +10,20 @@ interface IBlogCardSectionProps {
 const BlogCardSection: React.FC<IBlogCardSectionProps> = ({ posts }) => {
   return (
     <section className="grid grid-cols-1 gap-y-8 py-8 px-7 md:gap-y-6 md:overflow-hidden md:px-0 overflow-hidden box-border">
-      {posts.map((post: BlogPost, idx: number) => (
-        <React.Fragment key={post.id}>
-          <BlogCard post={post} />
-          {idx !== posts.length - 1 && (
-            <span className="md:hidden block h-1 bg-gray-800" />
-          )}
-        </React.Fragment>
-      ))}
+      {posts.length ? (
+        posts.map((post: BlogPost, idx: number) => (
+          <React.Fragment key={post.id}>
+            <BlogCard post={post} />
+            {idx !== posts.length - 1 && (
+              <span className="md:hidden block h-1 bg-gray-800" />
+            )}
+          </React.Fragment>
+        ))
+      ) : (
+        <span className="w-full text-gray-400">
+          현재 조건을 만족하는 포스트가 없습니다.
+        </span>
+      )}
     </section>
   );
 };
