ffd1a2bf9bbeb6cb93a399077b7e7d20abb39235
bjc1102 <bjc30849@gmail.com>
2022-11-04 21:34:17 +0900

    chore: 안쓰는 파일, 코드 삭제

--- components/Input.tsx
+++ components/Input.tsx
@@ -12,7 +12,6 @@ const Input: React.FC<ITextInputProps> = (props) => {
   return (
     <input
       className="border-2 text-sm rounded-lg block w-full p-2.5 pl-12 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-accent focus:border-accent outline-none"
-      placeholder="검색어를 입력하세요"
       {...props}
     />
   );
