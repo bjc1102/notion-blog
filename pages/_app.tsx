242a129e4bba31623080bc191d1e6588230f5b38
bjc1102 <bjc30849@gmail.com>
2023-08-17 16:05:19 +0900

    refactor(api): @tanstack/query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
 import Nav from '@/components/layout/Nav';
 import Footer from '@/components/layout/Footer';
-import { ReactQueryDevtools } from 'react-query/devtools';
 
 import '@/styles/global.css';
 import '@/styles/tailwind.style';
@@ -12,22 +17,16 @@ export default function CustomApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
-    <>
-      <Nav />
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <div className="block relative bg-primary text-white font-sans">
-            <React.Fragment>
-              <Component {...pageProps} />
-              <ReactQueryDevtools
-                initialIsOpen={false}
-                position="bottom-right"
-              />
-            </React.Fragment>
-          </div>
-          <Footer />
-        </Hydrate>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Nav />
+        <div className="block relative bg-primary text-white font-sans">
+          <React.Fragment>
+            <Component {...pageProps} />
+          </React.Fragment>
+        </div>
+        <Footer />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
